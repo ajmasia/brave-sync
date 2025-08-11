6afbb5008d8927032881b65dbce85418f73987b1
Antonio Masiá <ajmasia.dev@ysnp.link>
2025-08-11 12:44:18 +0200

    wip: add backup command

--- src/brave_sync/cli.py
+++ src/brave_sync/cli.py
@@ -2,10 +2,11 @@ import typer
 
 from .commands.config import register as register_config
 from .commands.version import register as register_version
+from .commands.backup import register as register_backup
 
 app = typer.Typer(help="Brave Sync CLI")
 
 
 # Register commands
-for reg in (register_version, register_config):
+for reg in (register_version, register_config, register_backup):
     reg(app)
